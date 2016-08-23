d6d5cc0b990ca471cfbee911d8d7ab9257e7fdfe
CainLuo <350116542@qq.com>
2016-08-23 17:35:22 +0800

    添加对应的工程, 以及对应的图片
    添加对应的工程, 以及对应的图片

---
+++ ObserverPattern/ObserverPattern工程/ObserverPattern/ObserverPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObserverPattern
+//
+//  Created by Cain on 23/8/16.
+//  Copyright © 2016年 Cain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObserverPattern/ObserverPattern工程/ObserverPattern/ObserverPattern/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ObserverPattern
+//
+//  Created by Cain on 23/8/16.
+//  Copyright © 2016年 Cain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
